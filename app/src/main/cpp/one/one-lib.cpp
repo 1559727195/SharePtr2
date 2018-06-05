7ba05518ef34ae777d74314dd0da3f6c9ca367d6
zhu <zhu@gmail.com>
2018-06-05 13:57:44 +0800

    原生代码调用java代码

--- app/src/main/cpp/one/one-lib.cpp
+++ app/src/main/cpp/one/one-lib.cpp
@@ -3,7 +3,34 @@
 //
 
 #include <jni.h>
-static  jmethodID  gOnNativeMessage = NULL;
+#include <pthread.h>
+
+
+// Native worker thread arguments
+struct NativeWorkerArgs {
+    jint id;
+    jint iterations;
+};
+
+// Method ID can be cached
+static jmethodID gOnNativeMessage = NULL;
+
+// Java VM interface pointer
+static JavaVM *gVm = NULL;
+
+// Global reference to object
+static jobject gObj = NULL;
+
+// Mutex instance
+static pthread_mutex_t mutex;
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    // Cache the JavaVM interface pointer
+    gVm = vm;
+
+    return JNI_VERSION_1_4;
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_massky_shareptr_JNIUtils_sayHelloFromJNI_1one_1lib(JNIEnv *env, jclass type) {
@@ -18,33 +45,73 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_massky_shareptr_MainActivity_nativeInit(JNIEnv *env, jobject instance) {
 
-    // TODO
-    if (NULL == gOnNativeMessage) {
-        jclass clazz = env->GetObjectClass(instance);
-        gOnNativeMessage = env->GetMethodID(clazz,
-                                            "onNativeMessage",
-                                            "Ljava/lang/String;)V");
+    // Initialize mutex
+    if (0 != pthread_mutex_init(&mutex, NULL)) {
+        // Get the exception class
+        jclass exceptionClazz = env->FindClass(
+                "java/lang/RuntimeException");
 
-        if (NULL == gOnNativeMessage) {
-            jclass exceptionClazz = env->FindClass(
-                    "java/lang/RuntimeException"
-            );
-            env->ThrowNew(exceptionClazz,
-                          "Unable to find method");
+        // Throw exception
+        env->ThrowNew(exceptionClazz, "Unable to initialize mutex");
+        goto exit;
+    }
+
+    // If object global reference is not set
+    if (NULL == gObj) {
+        // Create a new global reference for the object
+        gObj = env->NewGlobalRef(instance);
+
+        if (NULL == gObj) {
+            goto exit;
         }
     }
 
-}extern "C"
+    // If method ID is not cached
+    if (NULL == gOnNativeMessage) {
+        // Get the class from the object
+        jclass clazz = env->GetObjectClass(instance);
+
+        // Get the method id for the callback
+        gOnNativeMessage = env->GetMethodID(clazz,
+                                            "onNativeMessage",
+                                            "(Ljava/lang/String;)V");
+
+        // If method could not be found
+        if (NULL == gOnNativeMessage) {
+            // Get the exception class
+            jclass exceptionClazz = env->FindClass(
+                    "java/lang/RuntimeException");
+
+            // Throw exception
+            env->ThrowNew(exceptionClazz, "Unable to find method");
+        }
+    }
+
+    exit:
+    return;
+}
+
+extern "C"
 JNIEXPORT void JNICALL
 Java_com_massky_shareptr_MainActivity_nativeFree(JNIEnv *env, jobject instance) {
 
     // TODO
 
-}extern "C"
+}
+
+extern "C"
 JNIEXPORT void JNICALL
 Java_com_massky_shareptr_MainActivity_nativeWorker(JNIEnv *env, jobject instance, jint id,
                                                    jint interations) {
 
     // TODO
+
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_massky_shareptr_MainActivity_posixThreads(JNIEnv *env, jobject instance, jint threads,
+                                                   jint iterations) {
+
 
 }
--- app/src/main/java/com/massky/shareptr/MainActivity.java
+++ app/src/main/java/com/massky/shareptr/MainActivity.java
@@ -5,69 +5,151 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    /** Threads edit. */
+    private EditText threadsEdit;
+
+    /** Iterations edit. */
+    private EditText iterationsEdit;
+
+    /** Start button. */
     private Button startButton;
 
-    // Used to load the 'native-lib' library on application startup.
-//    static {
-//        System.loadLibrary("native-lib");
-//    }
+    /** Log view. */
+    private TextView logView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dev_model_time_select);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the native code
         nativeInit();
-        startButton = (Button) findViewById(R.id.startButton);
+
+        threadsEdit = (EditText) findViewById(R.id.threads_edit);
+        iterationsEdit = (EditText) findViewById(R.id.iterations_edit);
+        startButton = (Button) findViewById(R.id.start_button);
+        logView = (TextView) findViewById(R.id.log_view);
+
         startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int threads = 3;
-                int interations = 2;
-                if (threads > 0 && interations > 0) {
-                    startThreads(threads, interations);
+            public void onClick(View view) {
+                int threads = getNumber(threadsEdit, 0);
+                int iterations = getNumber(iterationsEdit, 0);
+
+                if (threads > 0 && iterations > 0) {
+                    startThreads(threads, iterations);
                 }
             }
         });
-
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    private native  void nativeInit();
-
-    private native  void nativeFree();
-
-    private native  void nativeWorker(int id, int interations);
-
-    static {
-        System.loadLibrary("one-lib");
-    }
-
-    private void startThreads (int threads, int iterations) {
-
     }
 
     @Override
     protected void onDestroy() {
+        // Free the native resources
         nativeFree();
-        super.onDestroy();
 
+        super.onDestroy();
     }
 
 
-    private void onNativeMessage (final String message) {
+    /**
+     * On native message callback.
+     *
+     * @param message
+     *            native message.
+     */
+    private void onNativeMessage(final String message) {
         runOnUiThread(new Runnable() {
-            @Override
             public void run() {
-                //
+                logView.append(message);
+                logView.append("\n");
             }
         });
+    }
+
+    /**
+     * Gets the value of edit text as integer. If the value
+     * is empty or count not be parsed, it returns the
+     * default value.
+     *
+     * @param editText edit text.
+     * @param defaultValue default value.
+     * @return numeric value.
+     */
+    private static int getNumber(EditText editText, int defaultValue) {
+        int value;
+
+        try {
+            value = Integer.parseInt(editText.getText().toString());
+        } catch (NumberFormatException e) {
+            value = defaultValue;
+        }
+
+        return value;
+    }
+
+    /**
+     * Starts the given number of threads for iterations.
+     *
+     * @param threads thread count.
+     * @param iterations iteration count.
+     */
+    private void startThreads(int threads, int iterations) {
+        posixThreads(threads, iterations);
+    }
+
+    /**
+     * Initializes the native code.
+     */
+    private native void nativeInit();
+
+    /**
+     * Free the native resources.
+     */
+    private native void nativeFree();
+
+    /**
+     * Native worker.
+     *
+     * @param id worker id.
+     * @param iterations iteration count.
+     */
+    private native void nativeWorker(int id, int iterations);
+
+    /**
+     * Using the POSIX threads.
+     *
+     * @param threads thread count.
+     * @param iterations iteration count.
+     */
+    private native void posixThreads(int threads, int iterations);
+
+    /**
+     * Using Java based threads.
+     *
+     * @param threads thread count.
+     * @param iterations iteration count.
+     */
+    private void javaThreads(int threads, final int iterations) {
+        // Create a Java based thread for each worker
+        for (int i = 0; i < threads; i++) {
+            final int id = i;
+
+            Thread thread = new Thread() {
+                public void run() {
+                    nativeWorker(id, iterations);
+                }
+            };
+
+            thread.start();
+        }
+    }
+
+    static {
+        System.loadLibrary("one-lib");
     }
 }
